03417b7a363f6e9d7a7bba088fdd6775b875bb3b
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-05-11 11:29:45 -0700

    initial commit

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int val = r.Next(1, 100);
+            int guess = 0;
+            bool correct = false;
+
+            Console.WriteLine("I'm thinking of a number between 1 and 100.");
+
+            while (!correct)
+            {
+                Console.Write("Guess: ");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out guess))
+                {
+                    Console.WriteLine("That's not a number.");
+                    continue;
+                }
+
+                if (guess > 100 || guess < 0)
+                {
+                    Console.WriteLine("That's not even in the right range!");
+                }
+                else if (guess < val)
+                {
+                    Console.WriteLine("No, the number I'm thinking is higher than that number.");
+                }
+                else if (guess > val)
+                {
+                    Console.WriteLine("No, the number I'm thinking is lower than that number.");
+                }
+                else
+                {
+                    correct = true;
+                    Console.WriteLine("You guessed right!");
+                }
+            }
+        }
+    }
+}
